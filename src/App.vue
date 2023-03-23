ddecc48713e7d642fdacffddcd7aa2e24420c0e2
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-23 20:57:33 +0100

    added Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,19 @@
-<script></script>
-<template></template>
+<template>
+  <Header/>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import Header from "@/components/Header.vue";
+
+export default defineComponent({
+  components: {
+    Header
+  }
+});
+</script>
+
+
+<style>
+</style>
