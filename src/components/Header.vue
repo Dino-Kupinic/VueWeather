e34561b10a4683f18a27a9cb6ae7e7ff481099ee
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-23 20:57:51 +0100

    defined Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <ul>
+      <li><h1>VueWeather</h1></li>
+      <li><input type="text"></li>
+      <li><button>?</button></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+      name: "Header"
+});
+</script>
+
+<style scoped>
+
+</style>
