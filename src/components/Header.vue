fca10d877e66280870360d64db0deda6b3fc776b
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-24 13:22:48 +0100

    abstracted the search bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,41 @@
 <template>
   <nav>
+    <h1>VueWeather</h1>
     <ul>
-      <li><h1>VueWeather</h1></li>
-      <li><input type="text"></li>
-      <li><button>?</button></li>
+      <li><Searchbar /></li>
+      <li>
+        <button>?</button>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import Searchbar from "@/components/Searchbar.vue";
 
 export default defineComponent({
-      name: "Header"
+  name: "Header",
+  components: {
+    Searchbar
+  }
 });
 </script>
 
 <style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 40px;
+  background: #cfd8dc;
+}
 
+nav ul {
+  display: flex;
+  list-style: none;
+}
+
+nav li {
+  padding-left: 20px;
+}
 </style>
