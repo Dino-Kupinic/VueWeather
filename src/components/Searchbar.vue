4f30f057ed6e1bc8022eb0396c496ead74b46246
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-27 19:34:00 +0200

    added binding and event emitter

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,11 +1,27 @@
 <template>
-  <input type="text" placeholder="Search...">
+  <input @change="passNewLocation" v-model="location.name" type="text" placeholder="Search...">
 </template>
 
-<script>
-export default {
-  name: "Searchbar"
+<script lang="ts">
+import {defineComponent} from "vue";
+
+interface location {
+  name: String;
 }
+
+export default defineComponent({
+  name: "Searchbar",
+  data() {
+    return {
+      location: {name: ""} as location
+    };
+  },
+  methods: {
+    passNewLocation() {
+      this.$emit("location-change", this.location);
+    }
+  },
+});
 </script>
 
 <style scoped>
