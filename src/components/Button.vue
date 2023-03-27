db138a8ce625dd2fa6f23f2834050ba96b49c5ad
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-27 19:23:49 +0200

    added Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Button",
+  props: {
+    text: String
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
