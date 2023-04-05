8d3ec597f270a39e140040a2d87071384764fd57
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-05 17:40:14 +0200

    small refactor, also changed @change to @input to update the value everytime the value changes instead of when enter is pressed

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,26 +1,22 @@
 <template>
-  <input @change="passNewLocation" v-model="location.name" type="text" placeholder="Search...">
+    <input @input="passNewLocation" v-model="location" type="text" placeholder="Search...">
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 
-interface location {
-  name: String;
-}
-
 export default defineComponent({
-  name: "Searchbar",
-  data() {
-    return {
-      location: {name: ""} as location
-    };
-  },
-  methods: {
-    passNewLocation() {
-      this.$emit("location-change", this.location);
-    }
-  },
+    name: "Searchbar",
+    data(): { location: string } {
+        return {
+            location: ""
+        };
+    },
+    methods: {
+        passNewLocation(): void {
+            this.$emit("location-change", this.location);
+        }
+    },
 });
 </script>
 
