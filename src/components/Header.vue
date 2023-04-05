4527658781349f6cbc7d9a8d351eb344ea2c4329
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-05 17:39:10 +0200

    addest test function and did some refactoring

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
-  <nav>
-    <h1>VueWeather</h1>
-    <ul>
-      <li>
-        <Searchbar @location-change="passNewLocation"/>
-      </li>
-      <li>
-        <Button text="?"/>
-      </li>
-    </ul>
-  </nav>
+    <nav>
+        <h1>VueWeather</h1>
+        <ul>
+            <li>
+                <Searchbar @location-change="passNewLocation"/>
+            </li>
+            <li>
+                <Button text="?" :func="test"/>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script lang="ts">
@@ -18,33 +18,36 @@ import Searchbar from "@/components/Searchbar.vue";
 import Button from "@/components/Button.vue";
 
 export default defineComponent({
-  name: "Header",
-  components: {
-    Button,
-    Searchbar
-  },
-  methods: {
-    passNewLocation(data: String) {
-      this.$emit("location-change", data);
+    name: "Header",
+    components: {
+        Button,
+        Searchbar
+    },
+    methods: {
+        passNewLocation(data: string) : void {
+            this.$emit("location-change", data);
+        },
+        test() {
+            console.log("testButton")
+        }
     }
-  }
 });
 </script>
 
 <style scoped>
 nav {
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 40px;
-  background: #cfd8dc;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 40px;
+    background: #cfd8dc;
 }
 
 nav ul {
-  display: flex;
-  list-style: none;
+    display: flex;
+    list-style: none;
 }
 
 nav li {
-  padding-left: 20px;
+    padding-left: 20px;
 }
 </style>
