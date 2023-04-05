7da9cecc62a6215cdcfad8c131a0ebf80a00927e
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-05 17:38:39 +0200

    made it possible to pass a function as a prop

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,16 @@
 <template>
-  <button>{{ text }}</button>
+    <button @click="func">{{ text }}</button>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 
 export default defineComponent({
-  name: "Button",
-  props: {
-    text: String
-  }
+    name: "Button",
+    props: {
+        text: String,
+        func: Function
+    }
 });
 </script>
 
