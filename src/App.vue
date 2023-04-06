5e2046713f34e3b200a3a57e9aa2beb5ddaf906e
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-06 23:36:51 +0200

    more styles and testing some functions

--- src/App.vue
+++ src/App.vue
@@ -13,32 +13,52 @@
 
 <script lang="ts">
 import {defineComponent, toRaw} from "vue";
+import {Location} from "@/scripts/location";
 import Header from "@/components/Header.vue";
+import {getTime} from "@/scripts/weather";
 
 export default defineComponent({
     components: {
         Header
     },
-    data(): { userInput: string } {
+    data(): { userInput: string, currentTime: string } {
         return {
-            userInput: ""
+            userInput: "",
+            currentTime: ""
         };
     },
     methods: {
-        getNewLocation(data: string) : void {
+        getNewLocation(data: string): void {
             this.userInput = toRaw(data);
+            this.currentTime = getTime();
+
+
+            const locationObject = new Location();
+            locationObject.name = this.userInput;
+            console.log(locationObject.name);
         }
-    }
+    },
 });
 </script>
 
 
 <style>
 body {
+    margin: 0;
+    background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+    background-size: 300% 300%;
+
     font-family: Arial, serif;
 }
 
-#location {
+#main-container {
     text-align: center;
+    font-weight: bold;
 }
+
+#location {
+    font-size: 60px;
+}
+
+
 </style>
