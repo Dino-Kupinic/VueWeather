6470c371a56881c822cdf6fc843af3645b8d8b70
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-06 23:38:19 +0200

    implementing fetchData() and getTime()

--- src/scripts/weather.ts
+++ src/scripts/weather.ts
@@ -1,6 +1,15 @@
+import type {Location} from "@/scripts/location";
 
-let API_KEY = "https://api.openweathermap.org/data/2.5/forecast?q= " + Location.name + "&appid=d134cb284ef6ebbab66ea57a6b83f4f8";
+const API_KEY_PART_1 : string = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const API_KEY_PART_2 : string = "&appid=d134cb284ef6ebbab66ea57a6b83f4f8";
 
-export default function fetchData() {
+export async function fetchData(location: Location) {
+    const finalAPIkey = API_KEY_PART_1 + location.name + "," + location.countryCode + API_KEY_PART_2;
+    fetch(finalAPIkey).then(data => data.json()).then(data => {
+        console.log(data);
+    });
+}
 
+export function getTime(): string {
+    return new Date().toDateString();
 }
