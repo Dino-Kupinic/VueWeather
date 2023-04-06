4d948add0c837e4a5cbf50db522a3dc4cf134e9f
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-06 23:37:20 +0200

    added Choicebox

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,9 @@
         <h1>VueWeather</h1>
         <ul>
             <li>
+                <Choicebox :country-list="countryCodes"/>
+            </li>
+            <li>
                 <Searchbar @location-change="passNewLocation"/>
             </li>
             <li>
@@ -16,30 +19,45 @@
 import {defineComponent} from "vue";
 import Searchbar from "@/components/Searchbar.vue";
 import Button from "@/components/Button.vue";
+import Choicebox from "@/components/Choicebox.vue";
 
 export default defineComponent({
     name: "Header",
     components: {
+        Choicebox,
         Button,
         Searchbar
     },
+    data(): { countryCodes: Array<string> } {
+        return {
+            countryCodes: [
+                "Austria, at", "United Kingdom, uk", "United States, us",
+                "Germany, de", "Japan, jp", "Canada, cn"
+            ]
+        };
+    },
     methods: {
-        passNewLocation(data: string) : void {
+        passNewLocation(data: string): void {
             this.$emit("location-change", data);
         },
         test() {
-            console.log("testButton")
+            console.log("testButton");
         }
     }
 });
 </script>
 
 <style scoped>
+h1 {
+    font-size: 30px;
+}
+
 nav {
     display: flex;
     justify-content: space-between;
-    padding: 20px 40px;
-    background: #cfd8dc;
+    padding: 20px 30px;
+    border-bottom: 20px solid #e7ffef;
+    background-color: white;
 }
 
 nav ul {
