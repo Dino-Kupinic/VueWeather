a6e850720bb833f9ba6902487b3a791e9f406afb
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-06 23:37:12 +0200

    added Choicebox

---
+++ src/components/Choicebox.vue
@@ -0,0 +1,30 @@
+<template>
+    <select>
+        <option disabled="disabled">please select a country</option>
+        <option v-for="code in countryList" :key="code"> {{ code }}</option>
+    </select>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "Choicebox",
+    data(): { selected: string } {
+        return {
+            selected: ""
+        };
+    },
+    props: {
+        countryList: Array
+    },
+    methods: {}
+});
+</script>
+
+<style scoped>
+select {
+    border-radius: 5px;
+    font-size: 22px;
+}
+</style>
