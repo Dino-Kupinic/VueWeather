75f4e819f607ff1b0ca713bb4f557b22d0eeb816
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-07 21:43:31 +0200

    added WEEK_DAYS

--- src/scripts/weather.ts
+++ src/scripts/weather.ts
@@ -1,7 +1,17 @@
 import type {Location} from "@/scripts/location";
 
-const API_KEY_PART_1: string = "https://api.openweathermap.org/data/2.5/forecast?q=";
-const API_KEY_PART_2: string = "&appid=d134cb284ef6ebbab66ea57a6b83f4f8";
+export const API_KEY_PART_1: string = "https://api.openweathermap.org/data/2.5/forecast?q=";
+export const API_KEY_PART_2: string = "&appid=d134cb284ef6ebbab66ea57a6b83f4f8";
+
+export const WEEK_DAYS: Array<string> = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
 
 export async function fetchData(location: Location) {
     const finalAPIkey = API_KEY_PART_1 + location.name + "," + location.countryCode + API_KEY_PART_2;
