4b3082bbf7f7a5d47c9e37d8bc3370b4259934d4
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-07 21:43:18 +0200

    added Day.vue

---
+++ src/components/Day.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "Day",
+    data(): { name: string, icon: string, temperatureInCelsius: number } {
+        return {
+            name: "",
+            icon: "",
+            temperatureInCelsius: 0,
+        };
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
