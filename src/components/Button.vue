0c3447196740a32805d3048835a04ed618e04016
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-05-05 23:53:01 +0200

    refactored to CompositionAPI

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,16 +2,11 @@
     <button @click="func">{{ text }}</button>
 </template>
 
-<script lang="ts">
-import {defineComponent} from "vue";
-
-export default defineComponent({
-    name: "Button",
-    props: {
-        text: String,
-        func: Function
-    }
-});
+<script setup lang="ts">
+defineProps<{
+    text?: string,
+    func?: Function
+}>()
 </script>
 
 <style scoped>
