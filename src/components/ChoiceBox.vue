d4b9de2e34409b229f720bf9099010cbb98d3261
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-05-05 23:53:07 +0200

    refactored to CompositionAPI

---
+++ src/components/ChoiceBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <select @input="locationStore.updateCountry(formatSelectedCountry)" v-model="selected">
+        <option disabled="disabled">please select a country</option>
+        <option v-for="code in countryList" selected :key="code"> {{ code }}</option>
+    </select>
+</template>
+
+<script setup lang="ts">
+import {computed, ref} from "vue";
+import {useLocationStore} from "@/stores/locationStore";
+
+const locationStore = useLocationStore();
+
+const selected = ref<string>("Austria, at");
+defineProps<{
+    countryList: string[]
+}>();
+
+const formatSelectedCountry = computed(() => {
+    return selected.value.substring(selected.value.indexOf(", ") + 2, selected.value.length);
+});
+
+</script>
+
+<style scoped>
+select {
+    padding: 4px;
+    border: 1px solid gray;
+    font-family: 'Roboto', sans-serif;
+    border-radius: 5px;
+    font-size: 20px;
+}
+</style>
