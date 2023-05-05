5db587105f85cc8b3b7a7111b8a154d6e5b33d64
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-05-05 23:54:44 +0200

    removed

--- src/scripts/logic.ts
+++
@@ -1,8 +0,0 @@
-import {Location} from "@/scripts/location";
-
-export function run(): void {
-    const testLocation = new Location("London", "uk");
-    console.log(testLocation.name);
-    console.log(testLocation.countryCode);
-
-}
