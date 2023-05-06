95e253ae7868270373849bb59a9612cf6a3e839d
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-05-06 20:53:00 +0200

    small changes

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,13 +1,25 @@
 <template>
-
+    <h1>{{ data.name }}</h1>
+    <h3>{{ data.averageTemperature }} °C</h3>
+    <p>Feels like {{ data.temperateFeelsLike }} °C</p>
+    <img v-bind:src="ImageURL" alt="noImage">
+    <h4>{{ data.currentWeather }}</h4>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import type {WeatherForecast} from "@/scripts/weather";
+import {fetchWeatherImage} from "@/scripts/weather";
+import {onMounted, ref} from "vue";
 
-const name = ref<string>("");
-const icon = ref<string>("");
-const temperatureInCelsius = ref<number>(0);
+const props = defineProps<{
+    data: WeatherForecast
+}>();
+
+const ImageURL = ref<string>("");
+
+onMounted(async () => {
+    ImageURL.value = await fetchWeatherImage(props.data.icon);
+})
 
 </script>
 
